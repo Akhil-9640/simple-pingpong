a421fb826f6406e9142979591a0013306fa03158
Akhil-9640 <akhilmunaganti@gmail.com>
2022-08-20 18:45:42 +0530

    added a racket(tl:2)

--- app.py
+++ app.py
@@ -17,7 +17,32 @@ screen = Screen()
 screen.setup(height=600,width=600)
 screen.bgcolor("black")
 screen.title("PONG")
+screen.tracer(0)
 
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.up()
+paddle.goto(-280, 0)
+
+
+def go_up():
+    new_y = paddle.ycor()+20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor()-20
+    paddle.goto(paddle.xcor(),new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 
