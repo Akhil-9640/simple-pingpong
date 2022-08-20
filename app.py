24703c9713c444fe2bfe189eff2c80d5742dd427
Akhil-9640 <akhilmunaganti@gmail.com>
2022-08-20 18:33:59 +0530

    Added a screen (tl:1)

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #Object class fot the racket; used two times
 #class for the scoreboard;
 #class for the ball
-#TODO 1: Create the screen
+#DONE-TODO 1: Create the screen
 #TODO 2: Create and move a paddle
 #TODO 3: Create another paddle
 #TODO 4: Create the ball and make it move
@@ -9,4 +9,21 @@
 #TODO 6: Detect Collision with the paddle
 #TODO 7: Detect when paddle misses
 #TODO 8: Keep Track of scores
+
+from turtle import Screen, Turtle
+import time
+
+screen = Screen()
+screen.setup(height=600,width=600)
+screen.bgcolor("black")
+screen.title("PONG")
+
+
+
+
+
+
+
+
+screen.exitonclick()
 
