256119b71afb8be4318aa9ce32a05074174503e1
Akhil-9640 <akhilmunaganti@gmail.com>
2022-08-20 19:51:26 +0530

    Implemented Racket as a class; created another racket(tl:3)

--- app.py
+++ app.py
@@ -1,54 +1,36 @@
-#Object class fot the racket; used two times
-#class for the scoreboard;
-#class for the ball
-#DONE-TODO 1: Create the screen
-#TODO 2: Create and move a paddle
-#TODO 3: Create another paddle
-#TODO 4: Create the ball and make it move
-#TODO 5: Detect collision with wall and bounce
-#TODO 6: Detect Collision with the paddle
-#TODO 7: Detect when paddle misses
-#TODO 8: Keep Track of scores
+# Object class fot the racket; used two times
+# class for the scoreboard;
+# class for the ball
+# DONE-TODO 1: Create the screen
+# TODO 2: Create and move a paddle
+# TODO 3: Create another paddle
+# TODO 4: Create the ball and make it move
+# TODO 5: Detect collision with wall and bounce
+# TODO 6: Detect Collision with the paddle
+# TODO 7: Detect when paddle misses
+# TODO 8: Keep Track of scores
 
 from turtle import Screen, Turtle
 import time
+from racket import Racket
 
 screen = Screen()
-screen.setup(height=600,width=600)
+screen.setup(height=600, width=600)
 screen.bgcolor("black")
 screen.title("PONG")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.up()
-paddle.goto(-280, 0)
-
-
-def go_up():
-    new_y = paddle.ycor()+20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor()-20
-    paddle.goto(paddle.xcor(),new_y)
+l_racket = Racket((-280, 0))
+r_racket = Racket((275, 0))
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_racket.go_up, "Up")
+screen.onkey(r_racket.go_down, "Down")
+screen.onkey(l_racket.go_up, "w")
+screen.onkey(l_racket.go_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
 
-
-
-
-
-
-
 screen.exitonclick()
-
