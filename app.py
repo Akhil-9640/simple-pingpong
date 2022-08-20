ec27ed39f993bb8ade80b5813bef8b2dad882dc3
Akhil-9640 <akhilmunaganti@gmail.com>
2022-08-20 18:16:50 +0530

    TODO list added

---
+++ app.py
@@ -0,0 +1,12 @@
+#Object class fot the racket; used two times
+#class for the scoreboard;
+#class for the ball
+#TODO 1: Create the screen
+#TODO 2: Create and move a paddle
+#TODO 3: Create another paddle
+#TODO 4: Create the ball and make it move
+#TODO 5: Detect collision with wall and bounce
+#TODO 6: Detect Collision with the paddle
+#TODO 7: Detect when paddle misses
+#TODO 8: Keep Track of scores
+
